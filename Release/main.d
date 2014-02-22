284d7ee39a94912ec80be2eea154b35f6d46e9ac
MrReinsch <Jester1685@gmx.de>
2014-02-22 19:52:51 +0100

    WallofLight - Kachel Rev.3.1

--- Release/main.d
+++
@@ -1,7 +0,0 @@
-main.d main.o: ../main.c \
- /home/developer/workspace/wol_kachel_rev_3/src/bam.h \
- /home/developer/workspace/wol_kachel_rev_3/src/transceive_data.h
-
-/home/developer/workspace/wol_kachel_rev_3/src/bam.h:
-
-/home/developer/workspace/wol_kachel_rev_3/src/transceive_data.h:
--- Release/src/bam.d
+++
@@ -1,5 +0,0 @@
-src/bam.d src/bam.o: ../src/bam.c ../src/bam.h ../src/transceive_data.h
-
-../src/bam.h:
-
-../src/transceive_data.h:
